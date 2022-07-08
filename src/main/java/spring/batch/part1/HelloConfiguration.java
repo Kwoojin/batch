dbec75f47b53fd84600e0742533d45ae78ca46a5
WoojinKim <dnwls488@naver.com>
2022-07-08 18:44:14 +0900

    batch 기초 테스트

---
+++ src/main/java/spring/batch/part1/HelloConfiguration.java
@@ -0,0 +1,45 @@
+package spring.batch.part1;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class HelloConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public HelloConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    /**
+     * RunIdIncrementer - Batch 실행 될때 마다 Parameter Id 생성
+     * start - 처음에 시작할 Step 지정
+     */
+    @Bean
+    public Job helloJob() {
+        return jobBuilderFactory.get("helloJob")
+                .incrementer(new RunIdIncrementer())
+                .start(helloStep())
+                .build();
+    }
+
+    @Bean
+    public Step helloStep() {
+        return stepBuilderFactory.get("helloStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("hello spring batch");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+}
